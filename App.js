abe60dd63821d9c35db2317d39d79b966a1c6066
Cristian V <cristianviar@gmail.com>
2021-10-27 15:36:07 +0200

    Add lives to state Create restart function Pass lives through header props

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ const App = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
   const [currentQuestion, setCurrentQuestion] = useState(questions[currentQuestionIndex])
 
+  const [lives, setLives ] = useState(5);
+
   //Cada vez que cambia el valor de currentQuestionIndex lo seteará en el estado
   useEffect(() => {
     if (currentQuestionIndex >= questions.length) {
@@ -27,13 +29,29 @@ const App = () => {
     setCurrentQuestionIndex(currentQuestionIndex + 1);
   }
 
+  const restart = () => {
+    setLives(5);
+    setCurrentQuestionIndex(0);
+  }
+
   const onWrong = () => {
     Alert.alert("Wrong!");
+    if (lives <= 1){
+      Alert.alert("Game over", "Try again", [
+        {
+          text: "Try again",
+          onPress: restart,
+        }
+      ]);
+    }else{
+      setLives(lives - 1);
+    }
+    
   }
 
   return (
     <View style={styles.root}>
-      <Header progress={currentQuestionIndex / questions.length} />
+      <Header progress={currentQuestionIndex / questions.length} lives={lives}/>
       {/* It should be a ternary expression image ? <ImageMultiple> : null but we can simplify it by
       adding &&. If currentQuestion.type === 'IMAGE_MULTIPLE_CHOICE' && is false whatever comes after will be false to
       true + false = false etc...
