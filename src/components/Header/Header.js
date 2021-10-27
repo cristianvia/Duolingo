16325c3d9a68dea13af762b2afdd5ddf807f9f93
Cristian V <cristianviar@gmail.com>
2021-10-27 15:36:23 +0200

    Add lives styles in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, Text } from "react-native";
 import ProgressBar from "../ProgressBar/ProgressBar";
+import heart from "../../../assets/images/heart.png"
 
-const Header = ({ progress }) => {
+const Header = ({ progress, lives }) => {
     return (
         <View style={styles.root}>
             <ProgressBar progress={progress} />
+            <Image source={heart} style={styles.icon} />
+            <Text style={styles.lives}>{lives}</Text>
         </View>
     )
 };
@@ -13,6 +16,18 @@ const Header = ({ progress }) => {
 const styles = StyleSheet.create({
     root: {
         flexDirection: "row",
+        alignSelf: "center",
+    },
+    icon:{
+        height:30,
+        width:30,
+        resizeMode: "contain",
+        marginHorizontal: 10,
+    },
+    lives:{
+        color:"red",
+        fontWeight: "bold",
+        fontSize: 18,
     }
 });
 
